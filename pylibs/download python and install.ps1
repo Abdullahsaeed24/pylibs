eaf0a10bb2d3f66f01bd7b7b4e9a370ca8cda26c
Abdullahsaeed24 <94451632+Abdullahsaeed24@users.noreply.github.com>
2022-12-02 10:19:46 +0100

    Add files via upload

---
+++ pylibs/download python and install.ps1
@@ -0,0 +1,12 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.10.4/python-3.10.4.exe" -OutFile "c:/temp/python-3.10.4.exe"
+
+c:/temp/python-3.10.4.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+setx PATH "%PATH%;C:\Python33\Scripts"
+
+Write-Host "Please press yes on the poped python window to continue the setup process!!"
+Start-Sleep -Seconds 60
+Read-Host -Prompt "Press any key to continue..."
+
+Exit
---
+++ pylibs/libs setup.ps1
@@ -0,0 +1,31 @@
+
+# Install a library using Pip
+
+python -m pip install numpy
+python -m pip install matplotlib
+python -m pip install seaborn
+python -m pip install httplib2
+python -m pip install cv
+python -m pip install apiclient
+python -m pip install statistics
+python -m pip install scipy
+python -m pip install math
+python -m pip install glob
+python -m pip install shutil 
+python -m pip install mimetypes 
+python -m pip install oauth2client
+python -m pip install apiclient.http
+python -m pip install datetime
+python -m pip install threading
+python -m pip install schedule
+python -m pip install win32gui
+python -m pip install win32con
+python -m pip install time
+python -m pip install --upgrade google-api-python-client
+python -m pip install pymap3d
+python -m pip install imageio
+python -m pip install shapely
+python -m pip --upgrade google-api-python-client
+
+Write-Host "setup finished !!"
+Exit
