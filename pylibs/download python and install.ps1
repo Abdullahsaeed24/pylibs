38cdbe8867c1652b156aae517a027dd740188a16
Abdullahsaeed24 <94451632+Abdullahsaeed24@users.noreply.github.com>
2022-12-08 15:48:46 +0100

    Update download python and install.ps1

--- pylibs/download python and install.ps1
+++ pylibs/download python and install.ps1
@@ -6,6 +6,16 @@ c:/temp/python-3.10.4.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
 setx PATH "%PATH%;C:\Python33\Scripts"
 
 Write-Host "Please press yes on the poped python window to continue the setup process!!"
+Write-Host " " 
+Write-Host "*******************************************************************************************"
+Write-Host "--------------------------------------- Please Wait ---------------------------------------"
+Write-Host "*******************************************************************************************"
+Write-Host " " 
+
+Write-Host "*******************************************************************************************"
+Write-Host "-----------------------------------Installation is Running --------------------------------"
+Write-Host "*******************************************************************************************"
+
 Start-Sleep -Seconds 60
 Read-Host -Prompt "Press any key to continue..."
 
